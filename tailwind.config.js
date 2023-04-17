f222261a5430e4a6c168263594f7e4b8358cb34b
Hassan Elwy <elwi.hassan@gmail.com>
2023-04-17 17:54:33 +0430

    Tailwindcss has been modified.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: [`./src/**/*.{js,jsx,ts,tsx}`],
   theme: {
     extend: {
       borderWidth: {
-        '0.5': '0.5px'
+        0.5: '0.5px',
       },
       colors: {
         'new-green': {
-          '500': '#20AF24'
+          500: '#20AF24',
         },
         'new-grey': {
-          '300': '#C1C1C1',
-          '700': '#707070'
-        }
+          300: '#C1C1C1',
+          700: '#707070',
+        },
       },
       fontSize: {
-        '3.2xl': '2rem'
-      }
+        '3.2xl': '2rem',
+      },
     },
   },
   plugins: [],
 }
-
