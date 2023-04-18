7543d2cc06c49829d374c01ddf9faadde8d94fc7
Hassan <hassan.zamani.75@gmail.com>
2023-04-18 10:17:11 +0430

    feat: implemented inputbox

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,23 +1,62 @@
-import React, { ReactElement } from 'react'
+import React, { ReactNode } from 'react'
 
-interface InputBoxInt extends ReactElement<HTMLInputElement> {
+interface InputProps extends React.HTMLProps<HTMLInputElement> {
   label?: string
-  placeholder?: string
+  themeColor?: 'gray' | 'white' | 'dark-2'
+  errorText?: string
+  rightIcon?: ReactNode
+  innerRightIcon?: ReactNode
+  inputSize?: 'large' | 'small' | 'medium'
 }
 
-const InputBox: React.FC<InputBoxInt> = ({ label = '', placeholder = '' }) => {
-  return (
-    <div className='flex flex-col'>
-      <label>{label} </label>
+const sizeMap = {
+  small: { input: 'py-1 px-2', label: 'text-sm' },
+  medium: { input: 'py-2 px-3', label: 'text-lg mb-2' },
+  large: { input: 'py-4 px-4', label: 'text-xl mb-4' },
+}
 
-      <input
-        type='text'
-        placeholder={placeholder}
-        className='outline-0 border-0.5 border-new-grey-300 rounded-sm
-                py-2.5 px-3 text-xs h-9 border-opacity-60'
-      />
-    </div>
+const themeMap = {
+  gray: {
+    input: 'bg-[#575757] border-1-[#A2A2A2] text-white',
+    label: 'text-white',
+  },
+  white: {
+    input: 'bg-[#FFFFFF] border border-[#4F5154] text-[#1B1B1B]',
+    label: 'text-[#1B1B1B]',
+  },
+  'dark-2': {
+    input: 'bg-[#3A3A3A] border border-[#707070] text-[#FFFFFF]',
+    label: 'text-[#FFFFFF]',
+  },
+}
+
+const defaultInputStyle = 'rounded-sm outline-none w-full flex items-center justify-center'
+
+const InputBox: React.FC<InputProps> = ({
+  label,
+  themeColor = 'gray',
+  errorText,
+  rightIcon,
+  inputSize = 'small',
+  innerRightIcon,
+  ...props
+}) => {
+  return (
+    <label className={`${themeMap[themeColor]?.label}`}>
+      <p className={`${sizeMap[inputSize].label}`}>{label}</p>
+      <div className='flex flex-row'>
+        <div
+          className={`${defaultInputStyle} ${themeMap[themeColor]?.input} ${sizeMap[inputSize].input} ${
+            errorText && 'border-red-500'
+          }`}
+        >
+          <input className={`bg-inherit outline-none w-full`} {...props} />
+          {innerRightIcon ?? <></>}
+        </div>
+        {rightIcon ?? <></>}
+      </div>
+      <div className='text-red-500 text-xs pl-1 min-h-[20px]'>{errorText}</div>
+    </label>
   )
 }
-
 export default InputBox
